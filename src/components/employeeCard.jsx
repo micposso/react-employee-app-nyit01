a01d79a56e6ab2a8fe70c141161d1a828feec68a
micposso <micposso@gmail.com>
2024-04-25 16:09:00 -0400

    loop data

--- src/components/employeeCard.jsx
+++ src/components/employeeCard.jsx
@@ -11,33 +11,35 @@ import Stack from "@mui/material/Stack";
 export default function MediaCard({ employee }) {
   console.log(employee);
   return (
-    <Card sx={{ maxWidth: 345, margin: 10, padding: 1 }}>
-      <CardMedia
-        sx={{ height: 200, backgroundSize: "contain", margin: 1 }}
-        image={employee[0].image}
-        title="profile"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {employee[0].firstName} {employee[0].lastName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {employee[0].department}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {employee[0].position}
-        </Typography>
-      </CardContent>
-
-      <Stack direction="row" spacing={1}>
-        <Chip label="Chip Filled" />
-        <Chip label="Chip Outlined" variant="outlined" />
-      </Stack>
-
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <>
+      {employee.map((item, index) => (
+        <Card key={index} sx={{ maxWidth: 345, margin: 10, padding: 1 }}>
+          <CardMedia
+            sx={{ height: 200, backgroundSize: "contain", margin: 1 }}
+            image={`/react-employee-app-nyit01/${item.image}`}
+            title="profile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.firstName} {item.lastName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.department}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.position}
+            </Typography>
+          </CardContent>
+          <Stack direction="row" spacing={1}>
+            <Chip label="Chip Filled" />
+            <Chip label="Chip Outlined" variant="outlined" />
+          </Stack>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </>
   );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -14,7 +14,7 @@ const data = [{
 
 {
   "id": "E1003",
-  "image": "profile1.png",
+  "image": "profile2.png",
   "firstName": "Lisa",
   "lastName": "White",
   "email": "lisa.white@example.com",
@@ -28,7 +28,7 @@ const data = [{
 
 {
   "id": "E1004",
-  "image": "profile1.png",
+  "image": "profile3.png",
   "firstName": "Mark",
   "lastName": "Brown",
   "email": "mark.brown@example.com",
@@ -42,7 +42,7 @@ const data = [{
 
 {
   "id": "E1005",
-  "image": "profile1.png",
+  "image": "profile4.png",
   "firstName": "Emily",
   "lastName": "Green",
   "email": "emily.green@example.com",
@@ -56,7 +56,7 @@ const data = [{
 
 {
   "id": "E1006",
-  "image": "profile1.png",
+  "image": "profile5.png",
   "firstName": "Alex",
   "lastName": "Johnson",
   "email": "alex.johnson@example.com",
